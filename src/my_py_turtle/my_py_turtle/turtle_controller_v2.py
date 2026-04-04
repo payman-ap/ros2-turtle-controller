62256a8a20201a4add8d4ebcef148ee758f7cef6
payman-ap <payman.azari@gmail.com>
2026-04-04 22:46:55 +0200

    Spiral movement and P-Controller added to the turtle_controller node

--- src/my_py_turtle/my_py_turtle/turtle_controller_v2.py
+++ src/my_py_turtle/my_py_turtle/turtle_controller_v2.py
@@ -19,16 +19,6 @@ class TurtleControllerNode(Node):
         self.alive_turtles = {}
         self.kill_distance_threshold = 0.6
 
-        self.pose_ = None  # To store turtle1's current position so the timer can access it
-
-        self.linear_vel = 2.0
-        self.radius = 0.5
-        self.angle_decrement = 0.02
-
-        self.p_gain = 1.5
-        self.angular_gain = 2.0
-        
-
         self.spawn_client_ = self.create_client(Spawn, "trigger_spawn")
         self.kill_client_ = self.create_client(Kill, "trigger_kill")
 
@@ -45,11 +35,8 @@ class TurtleControllerNode(Node):
         self.pose_subscriber_ = self.create_subscription(Pose, "/turtle1/pose", 
                                                          self.pose_callback, 10)
         self.pose_publisher_ = self.create_publisher(String, "/turtle1_pose", 10)
-        self.cmd_vel_publisher_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
 
-        self.timer_ = self.create_timer(0.25, self.control_loop)
-
-        self.move_timer_ = self.create_timer(0.1, self.move_to_closest_turtle)
+        self.timer_ = self.create_timer(1.5, self.control_loop)
 
         # Logging
         self.get_logger().info("Turtle Controller Node has started.")
@@ -58,70 +45,17 @@ class TurtleControllerNode(Node):
     def pose_callback(self, msg: Pose):
         # self.get_logger().info(f"Turtle pose: ({str(msg.x)} , {str(msg.y)}) with {str(msg.theta)} degrees")
         # self.get_logger().info(str(self.alive_turtles))
-        self.pose_ = msg
         pose_message = String()
         pose_message.data = f"Turtle pose: ({str(msg.x)} , {str(msg.y)}) with {str(msg.theta)} degrees"
         self.pose_publisher_.publish(pose_message)
         t1_x = msg.x
         t1_y = msg.y
-        # Eating turtle logic
         for name in list(self.alive_turtles.keys()):
             pos = self.alive_turtles[name]
             dist = math.sqrt((t1_x - pos[0])**2 + (t1_y - pos[1])**2)
+
             if dist < self.kill_distance_threshold:
                 self.kill_turtle(name)
-
-    def move_spiral(self):
-        cmd = Twist()
-        # Constant forward motion
-        cmd.linear.x = self.linear_vel
-        # Angular velocity decreases over time to expand the spiral
-        # msg.angular.z = v / r
-        cmd.angular.z = self.linear_vel / self.radius
-        self.radius += 0.01
-        self.cmd_vel_publisher_.publish(cmd)
-
-    def move_to_closest_turtle(self):
-        if self.pose_ is None:
-            return
-        if not self.alive_turtles:
-            # self.move_spiral()
-            self.get_logger().info("No turtles to hunt?")
-            return
-        
-        
-        # Find the closest turtle
-        t1_x = self.pose_.x
-        t1_y = self.pose_.y
-        closest_name = None
-        min_dist = math.inf
-        target_x, target_y = 0.0, 0.0
-
-        for name, pos in self.alive_turtles.items():
-            dist = math.sqrt((t1_x - pos[0])**2 + (t1_y - pos[1])**2)
-            if dist < min_dist:
-                target_x, target_y = pos[0], pos[1]
-                min_dist = dist
-                closest_name = name
-        
-        # Calculate the motion
-        target_angle = math.atan2(target_y - t1_y, target_x - t1_x)
-        steering_angle = target_angle - self.pose_.theta
-
-        # Normalize angle_error to stay between -pi and pi (prevents spinning 350 degrees)
-        steering_angle = math.atan2(math.sin(steering_angle), math.cos(steering_angle))
-
-        # Generating the Twist command
-        cmd = Twist()
-        if dist > 0.1:  # Tolerance: stop if we are 0.1 units away
-            # Proportional Control: move faster if far, slow down as you get closer
-            cmd.linear.x = self.p_gain * min_dist  
-            cmd.angular.z = self.angular_gain * steering_angle
-        else:
-            # Goal reached, Stop
-            cmd.linear.x = 0.0
-            cmd.angular.z = 0.0
-        self.cmd_vel_publisher_.publish(cmd)
 
 
     def control_loop(self):
